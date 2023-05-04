0e111e167ea6d5b2bae6d7fb1b7ca0871de1ba21
Ted Kozak <ted@yourfoodie.app>
2023-05-04 17:40:20 +1000

    Added some documentation

--- loader.js
+++ loader.js
@@ -1,24 +1,24 @@
 const _srcAttrib = "data-src"
 const _srcAttribPortrait = "data-portrait-src"
 
+// must match what is used in style.css as media query
+const _landscapeMinAR = 1.0
 
+// Main function that loads all videos. Useful with any video tag, not only the background. Can be simplified
+// if you're dealing just with the background video.
 function loadVids() {
     console.log('Fetching them videos')
-    let videos = allByTag('video')
+    let videos = document.getElementsByTagName('VIDEO')
     for (let i = 0; i < videos.length; i++) {
         _loadVid(videos[i])
     }
 }
 
-function isPortrait() {
-    let h = window.screen.availHeight
-    let w = window.screen.availWidth
-    return w / h < 1.0;
-}
 
 function _loadVid(video) {
 
     video.childNodes.forEach((srcNode) => {
+        // go through all the sources and pick appropriate src file - either portrait or landscape
         if (srcNode.hasAttribute && srcNode.hasAttribute(_srcAttrib)) {
             srcNode.src = _getVidSrc(srcNode)
         }
@@ -29,28 +29,32 @@ function _loadVid(video) {
         console.log(e.target.error.message)
     }
 
+    // Autoplay. Please note that if you're using this script on a page with more video tags, you may want
+    // to filter which videos are to be auto-played.
     function onLoaded(e) {
         video.play()
     }
 
     video.onerror = onErr
     video.onloadeddata = onLoaded
+
     video.load()
 }
 
 
 function _getVidSrc(sourceNode) {
-    if (isPortrait()) {
-        return sourceNode.getAttribute(_srcAttribPortrait)
+    if (_isLandscape()) {
+        return sourceNode.getAttribute(_srcAttrib)
     }
-    return sourceNode.getAttribute(_srcAttrib)
+    return sourceNode.getAttribute(_srcAttribPortrait)
 }
 
-function allByTag(tag) {
-    let tagClean = tag.toUpperCase()
-    let eles = document.getElementsByTagName(tagClean);
-    return eles;
+function _isLandscape() {
+    let h = window.screen.availHeight
+    let w = window.screen.availWidth
+    return w / h > _landscapeMinAR;
 }
 
 
+// Give a page some time to init before loading really heavy videos.
 window.setTimeout(loadVids, 500)
